f7ce6b97db43f8a944d5f077a0014fb94eff62fc
pro-mytest <165522383+pro-mytest@users.noreply.github.com>
2024-09-27 08:46:44 +0000

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,17 +4,27 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        stack = []
+        # Dictionary to map closing brackets to their corresponding opening ones
+        mapping = {')': '(', '}': '{', ']': '['}
 
+        for char in s:
+            if char in mapping:
+                # Pop the top element from the stack if it is not empty, otherwise assign a dummy value
+                top_element = stack.pop() if stack else '#'
 
+                # If the top element doesn't match the corresponding opening bracket, return False
+                if mapping[char] != top_element:
+                    return False
+            else:
+                # If it's an opening bracket, push it onto the stack
+                stack.append(char)
 
+        # If the stack is empty at the end, then the string is valid
+        return not stack
 
-
-
-
-    
-
-
-
-  
-
+# Example usage:
+solution = Solution()
+print(solution.isValid("()"))        # Output: True
+print(solution.isValid("()[]{}"))    # Output: True
+print(solution.isValid("(]"))        # Output: False
